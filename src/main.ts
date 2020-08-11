72ebddd9cc41c3cbff64f59f4729a66ce399e34b
Michael Holtzman <mholtzman@gmail.com>
2020-08-11 08:57:31 -0400

    feat: initial poc to render wsq diagrams

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {render} from './diagrams'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const folder: string = core.getInput('folder')
+    const style: string = core.getInput('format')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await render(folder, style)
   } catch (error) {
     core.setFailed(error.message)
   }
